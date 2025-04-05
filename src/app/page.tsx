790063107db292a696ee15da2a5cb1459ceb6a4a
Abdoul54 <abdelwahed.akhechane@gmail.com>
2025-04-05 20:05:32 +0000

    remove QueryProvider and related payment data fetching logic; update Home component to display a static title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,12 @@
 
-import { DataTable } from "@/components/data-table";
-import { columns } from "@/constants/columns/payments";
-import { usePayments } from "@/hooks/usePayments";
-
-
 export default function Home() {
-  const { data } = usePayments();
-
-  console.log("Payments data:", data);
-  
 
   return (
     <div className="container mx-auto p-4 ">
       <div className="overflow-hidden rounded-lg">
-        <DataTable
-          columns={columns}
-          data={data}
-        />
+        <h1>
+          Payments List
+        </h1>
       </div>
     </div>
   );
